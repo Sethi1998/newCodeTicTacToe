afbd300566487de4216e6a17cc3ee6d70418adb0
artidroid1190 <artiverma693@gmai.com>
2021-02-17 12:10:21 +0530

    Initial Commit

---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Soft Radix on 17/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
